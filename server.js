0c54780e1715b9e20d210cb95fcd105f9b069d14
Aayush Gupta <gayush794@gmail.com>
2024-02-05 01:44:39 +0530

    bug fix

--- server.js
+++ server.js
@@ -1,37 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
-
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
-// Import Mongo DB Atlas models
 const User = require('./models/user')
 const Exercise = require('./models/exercise')
 
-// Mount the body parser as middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// Connect Mongo DB Atlas
 mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 })
 
-// Enable cors for FCC to test the application
 app.use(cors())
 
-// Mount the middleware to serve the style sheets in the public folder
 app.use(express.static('public'))
-
-// Print to the console information about each request made
-app.use((req, res, next) => {
-  console.log(
-    'method: ' + req.method + '  |  path: ' + req.path + '  |  IP - ' + req.ip
-  )
-  next()
-})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
@@ -145,132 +131,53 @@ app.post('/api/users/:_id/exercises', (req, res) => {
 
 // PATH /api/users/:_id/logs?[from][&to][&limit]
 app.get('/api/users/:_id/logs', (req, res) => {
-  const id = req.body['_id'] || req.params._id
-  var fromDate = req.query.from
-  var toDate = req.query.to
-  var limit = req.query.limit
+  const userId = req.params._id
+  const { from, to, limit } = req.query
 
-  // Validate the query parameters
-  if (fromDate) {
-    fromDate = new Date(fromDate)
-    if (fromDate == 'Invalid Date') {
-      res.json('Invalid Date Entered')
-      return
+  // Validate the user ID
+  User.findById(userId, (err, user) => {
+    if (err) {
+      return res.status(500).json({ error: 'Internal Server Error' })
     }
-  }
-
-  if (toDate) {
-    toDate = new Date(toDate)
-    if (toDate == 'Invalid Date') {
-      res.json('Invalid Date Entered')
-      return
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
     }
-  }
 
-  if (limit) {
-    limit = new Number(limit)
-    if (isNaN(limit)) {
-      res.json('Invalid Limit Entered')
-      return
+    // Construct the filter for exercise logs
+    const logFilter = { username: user.username }
+    if (from || to) {
+      logFilter.date = {}
+      if (from) logFilter.date.$gte = new Date(from)
+      if (to) logFilter.date.$lt = new Date(to)
     }
-  }
 
-  // Get the user's information
-  User.findOne({ _id: id }, (error, data) => {
-    if (error) {
-      res.json('Invalid UserID')
-      return console.log(error)
-    }
-    if (!data) {
-      res.json('Invalid UserID')
-    } else {
-      // Initialize the object to be returned
-      const usernameFound = data.username
-      var objToReturn = { _id: id, username: usernameFound }
-
-      // Initialize filters for the count() and find() methods
-      var findFilter = { username: usernameFound }
-      var dateFilter = {}
-
-      // Add to and from keys to the object if available
-      // Add date limits to the date filter to be used in the find() method on the Exercise model
-      if (fromDate) {
-        objToReturn['from'] = fromDate.toDateString()
-        dateFilter['$gte'] = fromDate
-        if (toDate) {
-          objToReturn['to'] = toDate.toDateString()
-          dateFilter['$lt'] = toDate
-        } else {
-          dateFilter['$lt'] = Date.now()
+    // Find exercise logs
+    Exercise.find(logFilter)
+      .limit(limit ? parseInt(limit, 10) : undefined)
+      .exec((err, exercises) => {
+        if (err) {
+          return res.status(500).json({ error: 'Internal Server Error' })
         }
-      }
 
-      if (toDate) {
-        objToReturn['to'] = toDate.toDateString()
-        dateFilter['$lt'] = toDate
-        dateFilter['$gte'] = new Date('1960-01-01')
-      }
+        // Construct the response object
+        const logArray = exercises.map((exercise) => ({
+          description: exercise.description,
+          duration: exercise.duration,
+          date: exercise.date.toDateString(),
+        }))
 
-      // Add dateFilter to findFilter if either date is provided
-      if (toDate || fromDate) {
-        findFilter.date = dateFilter
-      }
-
-      // console.log(findFilter);
-      // console.log(dateFilter);
-
-      // Add the count entered or find the count between dates
-      Exercise.count(findFilter, (error, data) => {
-        if (error) {
-          res.json('Invalid Date Entered')
-          return console.log(error)
+        const responseObject = {
+          _id: userId,
+          username: user.username,
+          count: logArray.length,
+          log: logArray,
         }
-        // Add the count key
-        var count = data
-        if (limit && limit < count) {
-          count = limit
-        }
-        objToReturn['count'] = count
 
-        // Find the exercises and add a log key linked to an array of exercises
-        Exercise.find(findFilter, (error, data) => {
-          if (error) return console.log(error)
-
-          // console.log(data);
-
-          var logArray = []
-          var objectSubset = {}
-          var count = 0
-
-          // Iterate through data array for description, duration, and date keys
-          data.forEach(function (val) {
-            count += 1
-            if (!limit || count <= limit) {
-              objectSubset = {}
-              objectSubset.description = val.description
-              objectSubset.duration = val.duration
-              objectSubset.date = val.date.toDateString()
-              console.log(objectSubset)
-              logArray.push(objectSubset)
-            }
-          })
-
-          // Add the log array of objects to the object to return
-          objToReturn['log'] = logArray
-
-          // Return the completed JSON object
-          res.json(objToReturn)
-        })
+        res.json(responseObject)
       })
-    }
   })
 })
 
-// ----------------
-// ADDITIONAL PATHS (not required for the FreeCodeCamp project)
-
-// PATH /api/exercises/
-// Display all of the exercises in the Mongo DB model titled Exercise
 app.get('/api/exercises', (req, res) => {
   Exercise.find({}, (error, data) => {
     if (error) return console.log(error)
